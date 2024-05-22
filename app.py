aa3a545f1da76e10b593ec4d0114ec442082770a
willgao23 <williamtianyugao@gmail.com>
2024-05-22 10:36:23 -0700

    Add daily caching to minimize external API calls

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from lyricsgenius import Genius
 import os
 import math
+import datetime
 
 app = Flask(__name__)
 genius = Genius(os.environ['GENIUS_ACCESS_TOKEN'])
@@ -10,21 +11,24 @@ daily_game_state = {}
 
 @app.get("/game")
 def get_daily_game():
+    if not (daily_game_state and datetime.date.today().strftime("%B %d, %Y") == daily_game_state["date"]):
+        generate_daily_game("Blue sky")
     return daily_game_state
 
 def generate_daily_game(theme):
     request = genius.search_lyrics(theme)
+    daily_game_state.update({"date": datetime.date.today().strftime("%B %d, %Y")})
+    daily_game_state.update({"songs": {}})
     for hit in request['sections'][0]['hits']:
-        if len(daily_game_state) >= 4:
+        if len(daily_game_state["songs"]) >= 4:
             break
         title = hit['result']['full_title'].replace(u'\xa0', u' ')
-        # Prevents the inclusion of duplicates from translations / remixes
-        if (title.count('(') == 0):
+        if title.count('Remix') == 0 and title.count('Romanized') == 0:
             lyrics =  hit['highlights'][0]['value'].lower()
             words = lyrics.split()
             counter = math.ceil(len(words) / 4)
             grouped_words = [' '.join(words[i: i + counter]) for i in range(0, len(words), counter)]
             song_name = 'song_' + title[:3].upper()
             song = {"lyrics": grouped_words, "title": title}
-            daily_game_state.update({song_name: song})
+            daily_game_state["songs"].update({song_name: song})
     return

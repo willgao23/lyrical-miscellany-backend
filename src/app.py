62cba4e8f6a8a11247c6c9dbdd71744877f070b4
wgao23 <wgao23@students.cs.ubc.ca>
2024-07-08 03:38:50 -0700

    Update lyric algorithm

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ from better_profanity import profanity
 app = Flask(__name__)
 genius = Genius(os.environ['GENIUS_ACCESS_TOKEN'])
 DAILY_GAME_STATE = {}
-STARTING_DATE = date(2024, 7, 7)
+STARTING_DATE = date(2024, 6, 24)
 
 @app.route("/game", methods=["POST", "OPTIONS"])
 def get_daily_game():
@@ -25,7 +25,7 @@ def get_daily_game():
         return _corsify_actual_response(jsonify(DAILY_GAME_STATE))
     else:
         raise RuntimeError("Weird - don't know how to handle method {}".format(request.method))
-
+    
 def generate_daily_game(theme, date):
     request = search_genius_with_theme(theme)
     DAILY_GAME_STATE.update({"date": date.strftime("%B %d, %Y")})
@@ -44,7 +44,6 @@ def generate_daily_game(theme, date):
                 split_by_new_line = split_by_new_line[:-1]
             updated_group_words = []
             final_grouped_words = []
-            contains_no_short_sections = True
             for i in range(len(split_by_new_line)):
                 lyric_section = split_by_new_line[i]
                 lyric_section_words = lyric_section.split()
@@ -59,13 +58,13 @@ def generate_daily_game(theme, date):
                         partition_end = partition[2]
                         partition_start_count = len(partition_start.split())
                         partition_end_count = len(partition_end.split())
-                        if (partition_start_count > 1 and partition_end_count > 1):
+                        if (partition_start_count > 2 and partition_end_count > 2):
                             updated_group_words.append(partition_start)
                             updated_group_words.append(partition_end)
                             section_is_split = True
                 if not section_is_split:
                     if len(lyric_section_words) > 7:
-                        counter = math.floor(len(lyric_section_words) / 2)
+                        counter = math.ceil(len(lyric_section_words) / 2)
                         partition = [' '.join(lyric_section_words[i: i + counter])
                                         for i in range(0, 2 * counter, counter)]
                         updated_group_words.append(partition[0])
@@ -75,12 +74,10 @@ def generate_daily_game(theme, date):
             for j in range(len(updated_group_words)):
                 section = updated_group_words[j]
                 first_and_other_words = section.split(" ", 1)
-                if len(section.split()) < 3:
-                    contains_no_short_sections = False
                 if len(first_and_other_words[0]) == 1 and first_and_other_words[0] != "I":
                     section = first_and_other_words[1]
                 final_grouped_words.append(section.capitalize())
-            if len(final_grouped_words) >= 4 and contains_no_short_sections:
+            if len(final_grouped_words) >= 4:
                 song = {"lyrics": final_grouped_words[:4], "title": title}
                 DAILY_GAME_STATE["songs"].append(song)
 
@@ -108,4 +105,4 @@ def get_daily_game_state():
     return DAILY_GAME_STATE
 
 if __name__ == "__main__":
-   serve(app, host='0.0.0.0', port=5000)
+    serve(app, host='0.0.0.0', port=5000)
